c19934f53326e4be230f1fc63fc7bcc15393102a
Jessica Steen <jessicalsteen@yahoo.com>
2021-10-20 14:47:30 -0600

    problem 5

--- arrow.js
+++ arrow.js
@@ -80,7 +80,14 @@ const jsNinja = () => {
 */
 
 //CODE HERE
-
+const compareNums = (num1, num2) => {
+  if (num1 > num2) {
+    return num1
+  } else if (num1 < num2){
+    return num2
+  } else {
+  } return num1 
+}
   
 ////////// PROBLEM 6 //////////
 
