64dc33e34929459c6e2c46972ef37e70d553e03d
Jessica Steen <jessicalsteen@yahoo.com>
2021-10-22 00:00:26 -0600

    Problem 3

--- callbackAndHigherOrder.js
+++ callbackAndHigherOrder.js
@@ -61,15 +61,17 @@ first(names, firstName => {
 */
 
 // CODE HERE
-
+const last =(arr, callback) => {
+  callback(arr[names.length -1])
+}
 
 // UNCOMMENT THE FUNCTION CALL BELOW
 // RUN THIS FILE WITH NODE
 // CHECK YOUR ANSWER
 
-// last(names, lastName => {
-//   console.log('The last name in names is ' + lastName)
-// })
+last(names, lastName => {
+  console.log('The last name in names is ' + lastName)
+})
 
 
 
